bc103247c4f193c663bf2cfc44a0685883c6b79f
Paco Luna <pacoluna@Franciscos-MacBook-Pro.local>
2022-08-29 09:34:45 -0700

    Create helper methods for sessions

--- ruby_on_rails/seccion_3/alpha-blog/app/controllers/users_controller.rb
+++ ruby_on_rails/seccion_3/alpha-blog/app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
@@ -12,12 +12,9 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = 'Your account information was successfully updated'
       redirect_to @user
@@ -29,6 +26,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to tha Alpha blog #{@user.username}, you have a successfully sign up"
       redirect_to articles_path
     else
@@ -40,5 +38,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
